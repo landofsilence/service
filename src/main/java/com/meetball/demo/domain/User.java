dade3815fb1b6595c43b2aa5d3783dc1160a1455
jxnflzc <245186727@qq.com>
2019-08-29 16:56:34 +0800

    123

---
+++ src/main/java/com/meetball/demo/domain/User.java
@@ -0,0 +1,78 @@
+package com.meetball.demo.domain;
+
+import java.util.List;
+
+public class User {
+    private String userName;
+    private String name;
+    private String password;
+    private String carNumber;
+    private int age;
+    private boolean work;
+    private String telephone;
+    private List<String> listOrderId;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCarNumber() {
+        return carNumber;
+    }
+
+    public void setCarNumber(String carNumber) {
+        this.carNumber = carNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telphone) {
+        this.telephone = telphone;
+    }
+
+    public boolean isWork() {
+        return work;
+    }
+
+    public void setWork(boolean work) {
+        this.work = work;
+    }
+
+    public List<String> getListOrderId() {
+        return listOrderId;
+    }
+
+    public void setListOrderId(List<String> listOrderId) {
+        this.listOrderId = listOrderId;
+    }
+}
